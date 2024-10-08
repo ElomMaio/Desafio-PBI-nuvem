a2a0b3773469373a9efbe1f974d1ad71eb5dc583
ElomMaio <maioelom@gmail.com>
2024-10-08 13:25:11 -0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,14 +1,13 @@
--- Criar schema se não existir
-CREATE SCHEMA IF NOT EXISTS azure_company;
+SET DATESTYLE TO PostgreSQL, European;
+CREATE DATABASE azure_company
+    WITH 
+    TEMPLATE = template0 
+    ENCODING = 'UTF8' 
+    LC_COLLATE = 'en_US.utf8' 
+    LC_CTYPE = 'en_US.utf8';
 
--- Usar schema
-SET search_path TO azure_company;
-
--- Consultar constraints da tabela
-SELECT * FROM information_schema.table_constraints
-	WHERE constraint_schema = 'azure_company';
-
--- Criar tabela employee
+\c azure_company
+	
 CREATE TABLE employee (
 	Fname VARCHAR(15) NOT NULL,
 	Minit CHAR(1),
@@ -24,18 +23,15 @@ CREATE TABLE employee (
 	CONSTRAINT pk_employee PRIMARY KEY (Ssn)
 );
 
--- Adicionar foreign key
 ALTER TABLE employee 
 	ADD CONSTRAINT fk_employee 
 	FOREIGN KEY(Super_ssn) REFERENCES employee(Ssn)
 	ON DELETE SET NULL
 	ON UPDATE CASCADE;
 
--- Adicionar coluna com valor padrão
 ALTER TABLE employee 
 	ALTER COLUMN Dno SET DEFAULT 1;
 
--- Criar tabela department
 CREATE TABLE department (
 	Dname VARCHAR(15) NOT NULL,
 	Dnumber INT NOT NULL,
@@ -48,13 +44,11 @@ CREATE TABLE department (
 	FOREIGN KEY (Mgr_ssn) REFERENCES employee(Ssn)
 );
 
--- Modificar foreign key
 ALTER TABLE department 
 	DROP CONSTRAINT fk_dept, 
 	ADD CONSTRAINT fk_dept FOREIGN KEY(Mgr_ssn) REFERENCES employee(Ssn)
 	ON UPDATE CASCADE;
 
--- Criar tabela dept_locations
 CREATE TABLE dept_locations (
 	Dnumber INT NOT NULL,
 	Dlocation VARCHAR(15) NOT NULL,
@@ -62,14 +56,12 @@ CREATE TABLE dept_locations (
 	CONSTRAINT fk_dept_locations FOREIGN KEY (Dnumber) REFERENCES department (Dnumber)
 );
 
--- Modificar foreign key
 ALTER TABLE dept_locations 
 	DROP CONSTRAINT fk_dept_locations, 
 	ADD CONSTRAINT fk_dept_locations FOREIGN KEY (Dnumber) REFERENCES department(Dnumber)
 	ON DELETE CASCADE
 	ON UPDATE CASCADE;
 
--- Criar tabela project
 CREATE TABLE project (
 	Pname VARCHAR(15) NOT NULL,
 	Pnumber INT NOT NULL,
@@ -80,7 +72,6 @@ CREATE TABLE project (
 	CONSTRAINT fk_project FOREIGN KEY (Dnum) REFERENCES department(Dnumber)
 );
 
--- Criar tabela works_on
 CREATE TABLE works_on (
 	Essn CHAR(9) NOT NULL,
 	Pno INT NOT NULL,
@@ -90,7 +81,6 @@ CREATE TABLE works_on (
 	CONSTRAINT fk_project_works_on FOREIGN KEY (Pno) REFERENCES project(Pnumber)
 );
 
--- Criar tabela dependent
 DROP TABLE IF EXISTS dependent; -- Usar IF EXISTS para evitar erro se a tabela não existir
 CREATE TABLE dependent (
 	Essn CHAR(9) NOT NULL,
